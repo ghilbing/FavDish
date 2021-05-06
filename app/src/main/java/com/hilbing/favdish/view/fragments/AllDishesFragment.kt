9c7ca496760f1dd1ddf14191968928f54e010f6e
ghilbing <ghilbing@gmail.com>
2021-05-06 15:22:19 -0700

    Navigation component

--- app/src/main/java/com/hilbing/favdish/view/fragments/AllDishesFragment.kt
+++ app/src/main/java/com/hilbing/favdish/view/fragments/AllDishesFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.hilbing.favdish.R
 import com.hilbing.favdish.application.FavDishApplication
@@ -63,6 +64,10 @@ class AllDishesFragment : Fragment() {
     ): View? {
         mBinding = FragmentAllDishesBinding.inflate(inflater, container, false)
         return mBinding.root
+    }
+
+    fun dishDetails(){
+        findNavController().navigate(AllDishesFragmentDirections.actionNavigationAllDishesToDishDetailsFragment())
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
