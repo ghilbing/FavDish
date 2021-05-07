e8a03ea153e0588fedd26458b1be7b705568a2e1
ghilbing <ghilbing@gmail.com>
2021-05-07 10:20:18 -0700

    Loading favorites

--- app/src/main/java/com/hilbing/favdish/view/fragments/FavoriteDishesFragment.kt
+++ app/src/main/java/com/hilbing/favdish/view/fragments/FavoriteDishesFragment.kt
@@ -1,19 +1,28 @@
 package com.hilbing.favdish.view.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.hilbing.favdish.R
+import com.hilbing.favdish.application.FavDishApplication
 import com.hilbing.favdish.viewmodel.DashboardViewModel
+import com.hilbing.favdish.viewmodel.FavDishViewModel
+import com.hilbing.favdish.viewmodel.FavDishViewModelFactory
 
 class FavoriteDishesFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
+
+    private val  mFavDishViewModel : FavDishViewModel by viewModels {
+        FavDishViewModelFactory((requireActivity().application as FavDishApplication).repository)
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -28,5 +37,21 @@ class FavoriteDishesFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mFavDishViewModel.favoriteDishes.observe(viewLifecycleOwner){
+            dishes ->
+            dishes.let{
+                if(it.isNotEmpty()){
+                    for(dish in it){
+                        Log.i("Favorite Dishes", "${dish.id} :: ${dish.title}")
+                    }
+                } else {
+                    Log.i("Favorites is empty", "empty: ")
+                }
+            }
+        }
     }
 }
