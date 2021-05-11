85d7bcd48484bca251792c58abc2b69e29eb39a9
ghilbing <ghilbing@gmail.com>
2021-05-11 14:26:13 -0700

    Setting Spoonacular API and retrieving random dish

--- app/src/main/java/com/hilbing/favdish/view/fragments/RandomDishFragment.kt
+++ app/src/main/java/com/hilbing/favdish/view/fragments/RandomDishFragment.kt
@@ -1,6 +1,7 @@
 package com.hilbing.favdish.view.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,24 +10,56 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.hilbing.favdish.R
+import com.hilbing.favdish.databinding.FragmentRandomDishBinding
 import com.hilbing.favdish.viewmodel.NotificationsViewModel
+import com.hilbing.favdish.viewmodel.RandomDishViewModel
 
 class RandomDishFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private var mBinding: FragmentRandomDishBinding? = null
+
+    private lateinit var mRandomDishViewModel: RandomDishViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_random_dish, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        mBinding = FragmentRandomDishBinding.inflate(inflater, container,false)
+        return mBinding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mRandomDishViewModel = ViewModelProvider(this@RandomDishFragment).get(RandomDishViewModel::class.java)
+        mRandomDishViewModel.getRandomRecipeFromAPI()
+        randomDishViewModelObserver()
+        
+    }
+    
+    private fun randomDishViewModelObserver(){
+        mRandomDishViewModel.randomDishResponse.observe(
+            viewLifecycleOwner, 
+            {randomDishResponse -> randomDishResponse?.let{
+                Log.i("RANDOM DISH RESPONSE...", "${randomDishResponse.recipes[0]}")
+            }}
+        )
+        mRandomDishViewModel.randomDishLoadingError.observe(
+            viewLifecycleOwner,
+            {dataError -> dataError?.let {
+                Log.i("RANDOM DISH ERROR....", "$dataError")
+            }}
+        )
+        mRandomDishViewModel.loadRandomDish.observe(
+            viewLifecycleOwner,
+            { loadRandomDish -> loadRandomDish?.let {
+                Log.i("RANDOM DISH LOADING...", "$loadRandomDish")
+            }}
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
     }
 }
