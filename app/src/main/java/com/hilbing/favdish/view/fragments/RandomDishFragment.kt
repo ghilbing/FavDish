cedbfb31b9aef198bfd208bfd8fad91ab2d8e2b2
ghilbing <ghilbing@gmail.com>
2021-05-12 13:30:42 -0700

    Populating the UI with data from the API

--- app/src/main/java/com/hilbing/favdish/view/fragments/RandomDishFragment.kt
+++ app/src/main/java/com/hilbing/favdish/view/fragments/RandomDishFragment.kt
@@ -1,16 +1,28 @@
 package com.hilbing.favdish.view.fragments
 
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.hilbing.favdish.R
+import com.hilbing.favdish.application.FavDishApplication
 import com.hilbing.favdish.databinding.FragmentRandomDishBinding
+import com.hilbing.favdish.model.entities.FavDish
+import com.hilbing.favdish.model.entities.RandomDish
+import com.hilbing.favdish.utils.Constants
+import com.hilbing.favdish.viewmodel.FavDishViewModel
+import com.hilbing.favdish.viewmodel.FavDishViewModelFactory
 import com.hilbing.favdish.viewmodel.NotificationsViewModel
 import com.hilbing.favdish.viewmodel.RandomDishViewModel
 
@@ -42,6 +54,7 @@ class RandomDishFragment : Fragment() {
             viewLifecycleOwner, 
             {randomDishResponse -> randomDishResponse?.let{
                 Log.i("RANDOM DISH RESPONSE...", "${randomDishResponse.recipes[0]}")
+                setRandomDishResponseInUI(randomDishResponse.recipes[0])
             }}
         )
         mRandomDishViewModel.randomDishLoadingError.observe(
@@ -56,6 +69,72 @@ class RandomDishFragment : Fragment() {
                 Log.i("RANDOM DISH LOADING...", "$loadRandomDish")
             }}
         )
+    }
+
+    private fun setRandomDishResponseInUI(recipe: RandomDish.Recipe){
+        Glide.with(requireActivity())
+            .load(recipe.image)
+            .centerCrop()
+            .into(mBinding!!.ivDishImage)
+        mBinding!!.tvTitle.text = recipe.title
+        var dishType: String = "other"
+        if(recipe.dishTypes.isNotEmpty()){
+            dishType = recipe.dishTypes[0]
+            mBinding!!.tvType.text = dishType
+        }
+
+        mBinding!!.tvCategory.text = "other"
+        var ingredients = ""
+        for(value in recipe.extendedIngredients){
+            if(ingredients.isEmpty()){
+                ingredients = value.original
+            } else {
+                ingredients = ingredients + ", \n" + value.original
+            }
+        }
+        mBinding!!.tvIngredients.text = ingredients
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            mBinding!!.tvCookingDirection.text = Html.fromHtml(
+                recipe.instructions,
+                Html.FROM_HTML_MODE_COMPACT
+            )
+        } else {
+            @Suppress("DEPRECATION")
+            mBinding!!.tvCookingDirection.text = Html.fromHtml(recipe.instructions)
+        }
+
+        mBinding!!.tvCookingTime.text =
+            resources.getString(R.string.lbl_estimate_cooking_time, recipe.readyInMinutes.toString())
+
+        mBinding!!.ivFavoriteDish.setOnClickListener{
+            val randomDishDetails = FavDish(
+                recipe.image,
+                Constants.DISH_IMAGE_SOURCE_ONLINE,
+                recipe.title,
+                dishType,
+                "Other",
+                ingredients,
+                recipe.readyInMinutes.toString(),
+                recipe.instructions,
+                true
+            )
+
+            val mFavDishViewModel : FavDishViewModel by viewModels {
+                FavDishViewModelFactory((requireActivity().application as FavDishApplication).repository)
+            }
+            mFavDishViewModel.insert(randomDishDetails)
+            mBinding!!.ivFavoriteDish.setImageDrawable(
+                ContextCompat.getDrawable(
+                    requireActivity(),
+                    R.drawable.ic_favorite_selected
+                )
+            )
+            Toast.makeText(requireActivity(), resources.getString(R.string.msg_added_to_favorites), Toast.LENGTH_SHORT).show()
+        }
+
+
+
     }
 
     override fun onDestroy() {
